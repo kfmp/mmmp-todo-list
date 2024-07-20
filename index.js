ca8aa0bd7adb8507fd6840e1a15a015ce2e95510
sanghk32 <sanghk32@naver.com>
2024-07-20 17:41:33 +0900

    feat: ToDoList 구현

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
-const addToDoList = (e) => {
-  e.preventDefault();
-  const inputedData = document.querySelector('.text-data').value;
-  console.log(inputedData);
-  // 입력값의 앞뒤 공백을 제거하고 빈 값인지 비교
-  if (inputedData.trim() === '') {
-    alert('내용을 입력해주세요');
-  }
-};
+$(function() {
+  $('.asign').click(function(event) {
+    // 기본 동작 방지
+    event.preventDefault();
 
-const delToDoList = (e) => {
-  e.preventDefault();
-  if (confirm('삭제하시겠습니까?')) {
-    alert('삭제');
-  } else {
-    alert('취소');
-  }
-};
+    const inputTitle = $('.text-data').val();
+    if (inputTitle.trim() == '') {
+      alert('값을 입력해 주세요.')
+    } else {
+      // 새로운 항목을 생성
+      const newItem = `
+        <li class="todo-list-item">
+          <span class="todo-list-item-title">${inputTitle}</span>
+          <button class="alter-button">수정</button>
+          <button class="del-button">삭제</button>
+        </li>`
+      
+      // 항목을 추가
+      $('.todo-list').append(newItem);
+      alert('등록완료!');
+      $('.text-data').val('');
+    }
 
-const initEventListeners = () => {
-  document.querySelector('.asign').addEventListener('click', addToDoList);
-  document.querySelector('.del-button').addEventListener('click', delToDoList);
-};
+   $(function(){
+     $('.alter-button').click(function(e){
+      e.preventDefault();
+        const todoItem = $(this).closest('.todo-list-item');
+        const titleSpan = todoItem.find('.todo-list-item-title')
+        // 사용자로부터 새로운 제목 입력받기
+        const currentTitle= titleSpan.text();
+        const alterText = prompt('ToDoList 수정', currentTitle);
+    
+        // 사용자가 값을 입력한 경우에만 제목 변경
+        if (alterText !== null && alterText.trim() !== '') {
+          titleSpan.text(alterText.trim()); 
+          alert('수정완료!');
+        }
+      })
+    })
 
-initEventListeners();
-
-{
-  /* <li class="todo-list-item">
-  <div class="todo-list-item-title">글 제목 들어가는 곳</div>
-  <button class="alter-button">수정</button>
-  <button class="del-button">삭제</button>
-</li>; */
-}
-
-//addToDoList 에서 텍스트창에 입력한 내용을 저장 해놓는다.      V
-//게시글 목록이 하나 만들어진다.
-// 그 안에다가 입력한 내용을 붙여놓는다.
+    $(function(){
+      $('.del-button').click(function(e){
+        e.preventDefault();
+        if(confirm('삭제하시겠습니까?')){
+          const todoItem = $(this).closest('.todo-list-item');
+          todoItem.remove();
+          alert('삭제완료!');
+        }else{
+          alert('취소!')
+        }
+      })
+    })
+  });
+});
